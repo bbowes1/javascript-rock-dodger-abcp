afcb0bb723096edaea2135beba3adc08ece00d20
Benjamin Bowes <bbowes999@gmail.com>
2018-01-31 13:13:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ function checkCollision(rock) {
                 (rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge) ||
       (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge) ||
       (rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge)
+      
       return true
     }
   }
